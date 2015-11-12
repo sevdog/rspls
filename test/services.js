78253b394bd089906ab1777c40aa1ca26a0c9d83
sevdog <setti.davide89@gmail.com>
2015-11-12 23:08:23 +0100

    Added test for storageWrapper service

--- test/services.js
+++ test/services.js
@@ -1,6 +1,28 @@
+describe('Storage service', function() {
+	beforeEach(mockRSPLS());
+	it('Will save the value', inject(function(storageWrapper){
+		// first save the value
+		storageWrapper.set('string', 'test');
+		storageWrapper.set('number', 1);
+		storageWrapper.set('object', {foo: 'bar', baz: 1});
+		storageWrapper.set('array', [1,2,3]);
+	}, function(storageWrapper) {
+		expect(storageWrapper.get('string', '')).toEqual('test');
+		expect(storageWrapper.get('number', 0)).toEqual(1);
+		expect(storageWrapper.get('object', {})).toEqual({foo: 'bar', baz: 1});
+		expect(storageWrapper.get('array', [])).toEqual([1,2,3]);
+	}));
+
+	it('Will get default value when missing', inject(function(storageWrapper) {
+		expect(storageWrapper.get('string', '')).toEqual('');
+		expect(storageWrapper.get('number', 0)).toEqual(0);
+		expect(storageWrapper.get('object', {})).toEqual({});
+		expect(storageWrapper.get('array', [])).toEqual([]);
+	}));
+});
 
 describe('Score service test', function() {
-	beforeEach(module('rspls'));
+	beforeEach(mockRSPLS());
 	it('Win 1 - Tie 0 - Lost 0 - Situation > 0', inject(function(score) {
 		score.addGame(true, true)
 		expect(score.win).toEqual(1);
@@ -25,7 +47,7 @@ describe('Score service test', function() {
 });
 
 describe('CPU service test', function() {
-	beforeEach(module('rspls'));
+	beforeEach(mockRSPLS());
 	describe('With default settings', function() {
 		it('Will use all signs', inject(function(cpu, rules) {
 			var randomSpy = spyOn(Math, 'random');
